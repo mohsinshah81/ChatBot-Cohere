0001f7e1fb221e5b41f6b54b5bf7664b39ef6e46
mohsinshah81 <mohs.shah81@gmail.com>
2026-01-06 18:45:46 -0500

    streamlit-chat.py

---
+++ streamlit-chat.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import cohere
+
+with st.sidebar:
+  API_KEY = st.text_input("COHERE API KEY", key="chatbot_api_key", type="password")
+
+st.title("Cohere Chatbot")
+
+# Chat history
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "CHATBOT", "message": "Can I help?"}]
+
+# Display history
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["message"])
+
+prompt = st.chat_input()
+
+if prompt:
+
+  if not API_KEY:
+    st.info("please add Cohere API Key")
+    st.stop()
+
+    client = cohere.Client(API_KEY)
+    st.chat_message("USER").write(prompt)
+
+    response = client.chat(chat_history=st.session_state.messages, message=prompt)
+    st.session_state.messages.append({"role": "USER", "message": prompt})
+    msg = response.text
+
+    st.session_state.messages.append({"role": "CHATBOT", "message": msg})
+    st.chat_message("ChatBOT".write(msg))
